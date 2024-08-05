aa5fd34d73dbb2ec490e5c0edf33e2dd10f85b53
Moonsu-11 <joey300@naver.com>
2024-08-06 02:15:36 +0900

    FEAT : 학습하기
    학습하기 제작 완료

--- Assets/Scripts/ClearCanvas.cs
+++ Assets/Scripts/ClearCanvas.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using BeyondLimitsStudios.VRInteractables;
 
 public class ClearCanvas : MonoBehaviour
 {
     public GameObject[] objectsToHide; // 비활성화할 오브젝트 배열
     public Button[] buttons; // 버튼 배열
+    public Data data;
 
     void Start()
     {
@@ -29,7 +31,13 @@ public class ClearCanvas : MonoBehaviour
     {
         foreach (GameObject obj in objectsToHide)
         {
-            obj.SetActive(false); // 오브젝트를 비활성화
+            obj.SetActive(false); // 오브젝트를 비활성ghk
         }
+        data.correctCube = 0;
+        data.wrongCube = 0;
+        data.totalAnswerCube = 0;
+        data.missedCube = 0;
+        data.accuracy = 0;
+        DrawingBoardTexture.clearAll();
     }
 }
